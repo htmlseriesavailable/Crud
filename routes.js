56e4bcde3bc89d6c47c376de018ddbc046aa7e5a
Sushmita Sarle <147891161+htmlseriesavailable@users.noreply.github.com>
2023-11-18 18:25:44 +0530

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const router = express.Router();
+const user = require('../models/users');
+const multer = require('multer');
+const fs = require('fs');
+
+//image upload
+var storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, './upload')
+  },
+  filename: function(req, file, cb){
+    cb(null, file.fieldname +"_"+ Date.now() + "_" + file.originalname);
+  },
+})
+var upload = multer({
+  storage: storage,
+}).single('image');
+
+//Insert an user into database route
+router.post('/add', upload, (req, res)=>{
+  const user = new user({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    image: req.file.filename,
+  });
+  user.save((err) => {
+    if(err){
+      res.json({message: err.message,type: 'danger'});
+    }else{
+      req.session.message = {
+        type: 'success',
+        message: 'User added successfully'
+      };
+      res.redirect("/");
+    }
+
+  });
+});
+//Get all users route
+
+router.get("/",(req, res) => {
+user.find().exec((err, users)=> {
+  if(err){
+    res.json({ message: err.message });
+  }else {
+    res.render("index", {
+      title:"Home Page",
+      users: users,
+    });
+  }
+});
+});
+
+router.get("/add", (req, res) => {
+    res.render("add_users", {title: "Add Users"});
+});
+
+//Edit an user route
+router.get('/edit/:id', (req, res) => {
+let id = req.params.id;
+user.findById(id, (err, user)=> {
+  if(err){
+    res.redirect('/');
+  } else{
+    if(user == null){
+      res.redirect('/');
+    }else{
+      res.render('edit_users', {
+        title: "Edit User",
+        user: user,
+      });
+    }
+  }
+});
+});
+//Update user route
+router.post('/update/:id', upload, (req, res) => {
+  let id = req.params.id;
+  let new_image = "";
+
+if(req.file){
+  new_image = req.file.filename;
+  try{
+    fs.unlinkSync('./uploads/'+req.body.old_image);
+  }catch(err){
+    console.log(err);
+  }
+}else{
+  new_image = req.body.old_image;
+}
+
+User.findByIdAndUpdate(id, {
+  name: req.body.name,
+  email: req.body.email,
+  phone: req.body.phone,
+  image: new_image,
+}, (err, result) => {
+  if(err){
+    res.json({ message: err.message, type: 'danger'});
+  } else {
+    req.session.message = {
+      type: 'succes',
+      message: 'User Updated successfully!',
+    };
+    res.redirect("/");
+  }
+})
+});
+  
+// Delete user route
+router.get('/delete/:.id', (req, res) => {
+  let id = req.params.id;
+  user.findByIdAndRemove(id, (err, result) => {
+    if(result.image != ''){
+      try{
+        fs.unlinkSync('./uploads/'+result.image);
+      } catch(err){
+        console.log(err);
+      }
+    }
+
+    if(err){
+      res.json({message: err.message});
+    } else {
+      req.session.message = {
+        type: 'info',
+        message: 'User deleted successfully!',
+      };
+      res.redirect("/");
+    }
+  })
+})
+module.exports = router;
